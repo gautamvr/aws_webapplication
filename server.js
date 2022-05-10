607ac510233f82af3cf23340a6a7aee6fb4bf597
Gautam VR <gautamvr55@gmail.com>
2022-05-10 23:57:57 +0530

    Corrected open endpoints

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-const express = require("express")
-
-const app = express();
-const postsRoute = require("./routes/posts")
+const app = require('./app.js')
 
 const PORT = 8080;
-
-app.use('/',postsRoute);
 
 app.listen(PORT,()=>{
     console.log(`Started Listening on port ${PORT}`);
 })
-
-module.exports = app
